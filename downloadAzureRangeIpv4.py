a153e92d6ef71451d09a33abf74fcacf772be367
TobiASS <46230851+TobiasS1402@users.noreply.github.com>
2022-02-01 16:01:45 +0100

    Create downloadAzureRangeIpv4.py

---
+++ downloadAzureRangeIpv4.py
@@ -0,0 +1,22 @@
+import requests
+import json
+import re
+
+#Download Azure ip list https://www.microsoft.com/en-us/download/details.aspx?id=56519
+url = requests.get('https://download.microsoft.com/download/7/1/D/71D86715-5596-4529-9B13-DA13A5DE5B63/ServiceTags_Public_20220131.json') #all public ip-ranges in azure
+txt = url.text
+data = json.loads(txt)
+
+list = []    
+
+for x in data["values"]:
+    for y in x["properties"]["addressPrefixes"]:
+        list.append(y)
+
+#Massscan does not like ipv6
+file=open('AzureIPRanges.txt', 'w+')
+for iprange in list:
+    if not re.search(r'(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))', iprange):
+        file.writelines(iprange+'\n')
+
+file.close()
